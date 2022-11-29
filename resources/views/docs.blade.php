a4424816e957a77d08088a1621f9bf68c4b1ddca
Tony Messias <tonysm@hey.com>
2022-11-28 21:47:37 -0300

    Remove header

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -59,43 +59,6 @@
                 </aside>
 
                 <main id="main-content" class="flex-1 py-4 px-2 lg:px-24 lg:py-24 bg-white min-h-screen prose prose-pre:p-0 prose-pre:mr-4 w-full max-w-4xl">
-                    @if (! $page || $page === 'introduction')
-                        <div class="relative -mr-22 xl:-mr-44 mb-12 sm:mb-20 xl:flex xl:justify-between xl:items-center">
-                            <div>
-                                <h1>
-                                    <small class="text-xl font-medium leading-10 text-gray-900 dark:text-white">Learn</small>
-                                    <br />
-                                    <span class="mt-4 text-4xl sm:text-5xl font-bold text-gray-900 dark:text-white">Hotwire</span>
-                                    <br />
-                                    <span class="text-4xl sm:text-5xl font-bold text-red-600">for Web Artisans</span>
-                                </h1>
-                                <div class="mt-6 xl:mt-8 relative xl:static">
-                                    <div class="mb-6 sm:mt-0 absolute bottom-0 xl:static">
-                                        <div class="xl:-mr-24 p-3 sm:p-6 flex sm:inline-flex xl:flex space-x-2 rounded-lg border dark:border-none relative bg-white dark:bg-dark-500 shadow-xl z-[1]">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                                            </svg>
-                                            <div class="flex-1">
-                                                <div class="flex justify-between">
-                                                    <div>
-                                                        <span class="text-sm font-medium sm:text-sm text-gray-800 dark:text-gray-200">Tony Messias</span>
-                                                        <br class="sm:hidden">
-                                                        <small class="sm:ml-2 text-xs sm:text-sm text-gray-600 dark:text-gray-500">just now</small>
-                                                    </div>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                    </svg>
-                                                </div>
-                                                <p class="mt-2 sm:mt-4 mr-1 sm:mr-0 text-sm sm:text-md text-gray-900 dark:text-gray-100">Let's build something with Turbo Laravel!</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <img src="{{ asset('/images/intro-code-showcase.png') }}" alt="Example Code" class="hidden xl:block rotate-3 rounded-md w-[450px]">
-                        </div>
-                    @endif
-
                     @include($page)
                 </main>
             </div>
